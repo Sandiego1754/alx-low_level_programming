327c142f78da72af2a3a28282752e1541d0ec299
Sandiego1754 <sandiego4god@gmail.com>
2023-04-26 06:37:54 -0700

    print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+#define MAIN_H
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -12,7 +12,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _purchar(char c);
+int _putchar(char c);
 void print_times_table(int n);
 
 #endif
