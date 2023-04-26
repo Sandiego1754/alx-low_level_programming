fffb425c0b73e2a37fc47efc1a6dd641c34f0a43
Sandiego1754 <sandiego4god@gmail.com>
2023-04-26 04:52:57 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,18 @@
 #ifndef MAIN-H
 #define MAIN-H
 
-void print-alphabet(void);
-void print-alphabet_x10(void);
-int -islower(int c);
-int -isalpha(int c);
-int -print_sign(int n);
-int -abs(int);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 int _purchar(char c);
+void print_times_table(int n);
 
 #endif
