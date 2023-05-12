4b77ad2eb7054d36cfe66273cc52aa6cf60a5973
Sandiego1754 <sandiego4god@gmail.com>
2023-05-12 13:53:25 -0700

    all task completed

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+My readme file for the project on 0x09-static_libraries
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unstid.h>
+
+/**
+ * _putchar - write the character c to stand out
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
